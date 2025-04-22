583cfcc2424bd3f81391fa67bab069b0035eeed8
Gabrielygor <gabrielcanuto042@gmail.com>
2025-04-22 19:48:33 -0300

    feat: add login route

--- traininSync-front/src/routes/routes.tsx
+++ traininSync-front/src/routes/routes.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import App from "../App";
 import { RegisterForm } from "../pages/register";
 import { NotFound } from "../pages/notFound";
+import { Login } from "../pages/login";
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="*" element={<NotFound />} /> {/* Pagina de erro 404 (Not Found)  */}
+      <Route path="*" element={<NotFound />} />{" "}
       <Route path="/" element={<App />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/register" element={<RegisterForm />} />
     </Routes>
   );
