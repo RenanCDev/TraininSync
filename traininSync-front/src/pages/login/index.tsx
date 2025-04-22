f30be489911b95fad2120e9ebf2480cb0557b254
Gabrielygor <gabrielcanuto042@gmail.com>
2025-04-22 19:49:08 -0300

    feat: add login screen inital page

---
+++ traininSync-front/src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+// import { Button } from "../../components/button";
+
+export function Login() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      {/* <div className="hidden bg-[url('/login-image.png')] bg-cover h-screen text-white sm:block w-1/2"></div> */}
+      <h1>Login Page</h1>
+
+    </div>
+  );
+}
