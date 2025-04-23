f878f1a154968bb334b666ff190a392e8a1dac25
Gabrielygor <gabrielcanuto042@gmail.com>
2025-04-23 09:34:53 -0300

    refactor: add link in navbar logo

--- traininSync-front/src/components/navbar/index.tsx
+++ traininSync-front/src/components/navbar/index.tsx
@@ -1,14 +1,26 @@
 import { ReactNode } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface NavBarProps {
   children?: ReactNode;
 }
 
 export function NavBar({ children }: NavBarProps) {
+  const navigate = useNavigate();
+
+  function handleLogoClick() {
+    navigate("/");
+  }
+
   return (
     <div className="flex justify-center items-center py-8 px-14 border-b border-midGray top-0 sm:justify-between relative z-10">
       <div className="absolute inset-0 bg-gradient-to-b from-black/60 to-transparent pointer-events-none"></div>
-      <div className="text-4xl font-extrabold">TraininSync</div>
+      <div
+        className="text-4xl font-extrabold cursor-pointer"
+        onClick={handleLogoClick}
+      >
+        TraininSync
+      </div>
       <div className="hidden gap-7 text-2xl md:flex">
         <p className="cursor-pointer transition-colors duration-300 hover:text-midPurple animate-fade-in-up">
           Home
