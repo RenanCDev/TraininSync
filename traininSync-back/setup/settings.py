5c2677e3d2690789dc2245037898c13be8d42d78
luizmiguel4444 <luizmiguel4444@gmail.com>
2025-04-23 20:03:39 -0600

    refactor: necessary changes for deploy

--- traininSync-back/setup/settings.py
+++ traininSync-back/setup/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,14 +82,9 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bdtraininsync',
-        'USER': 'admin',
-        'PASSWORD': 'senha-traininsync',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
 
 
