ab0e191a0c05d5f9ad22c3c834e6357f18d584fa
luizmiguel4444 <luizmiguel4444@gmail.com>
2025-04-23 20:05:37 -0600

    refactor: necessary changes for deploy

--- traininSync-back/setup/urls.py
+++ traininSync-back/setup/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("api/traininsync/", include("traininsync.urls", namespace="traininsync")),
 
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
