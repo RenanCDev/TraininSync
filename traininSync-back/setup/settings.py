885c0b2fff32473e9c767716ef31eaa1576d733b
luizmiguel4444 <luizmiguel4444@gmail.com>
2025-04-24 21:14:34 -0600

    fix: modify database settings

--- traininSync-back/setup/settings.py
+++ traininSync-back/setup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,14 +79,9 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bdtraininsync',
-        'USER': 'admin',
-        'PASSWORD': 'senha-traininsync',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 
