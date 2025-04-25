c0bbed19da15ff1d824dbfea37e271434e725010
raelaraujo0 <raellyedson1@gmail.com>
2025-04-25 09:49:03 -0300

    feat: little change in servico model and add urls and views for aluno n servico that i forget #51

--- traininSync-back/traininsync/urls.py
+++ traininSync-back/traininsync/urls.py
@@ -1,9 +1,13 @@
 from rest_framework.routers import SimpleRouter
 
-from .views import PersonalViewSet
+from .views import PersonalViewSet, AlunoViewSet, ServicoViewSet
 
 
 app_name = "traininsync"
 router = SimpleRouter()
-router.register(r"", PersonalViewSet, basename="traininsync")
+
+router.register(r"personais", PersonalViewSet, basename="personais")
+router.register(r"alunos", AlunoViewSet, basename="alunos")
+router.register(r"servicos", ServicoViewSet, basename="servicos")
+
 urlpatterns = router.urls
