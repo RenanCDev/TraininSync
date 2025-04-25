2a18a5c52d69b9acb46b13a16eb2a7c858c67748
Gabrielygor <gabrielcanuto042@gmail.com>
2025-04-25 07:02:24 -0300

    feat: add register screen

--- traininSync-front/src/pages/register/index.tsx
+++ traininSync-front/src/pages/register/index.tsx
@@ -2,11 +2,11 @@ import { useNavigate } from "react-router-dom";
 import { ArrowBack } from "../../components/arrow-back";
 import { Button } from "../../components/button";
 
-export function Login() {
+export function Register() {
   const navigate = useNavigate();
 
-  function handleRedirectToRegister() {
-    navigate("/register");
+  function handleRedirectToLogin() {
+    navigate("/login");
   }
 
   return (
@@ -19,7 +19,7 @@ export function Login() {
         </div>
 
         <div className="flex flex-col gap-5 w-full m-10 md:max-w-1/2">
-          <h1 className="text-5xl font-black">Login</h1>
+          <h1 className="text-5xl font-black">Cadastro</h1>
           <div className="border border-midPurple rounded-3xl text-lowGray flex flex-col gap-4 p-5">
             <div className="flex flex-col gap-2">
               <h2>Usuário</h2>
@@ -29,17 +29,18 @@ export function Login() {
               <h2>Senha</h2>
               <div className="h-11 bg-midGray rounded-xl"></div>
             </div>
-            <p className="flex justify-end cursor-pointer hover:text-darkPurple transition-colors duration-300 ease-in-out">
-              Esqueci minha senha
-            </p>
+            <div className="flex flex-col gap-2">
+              <h2>Confirmar Senha</h2>
+              <div className="h-11 bg-midGray rounded-xl"></div>
+            </div>
           </div>
           <div className="flex flex-col justify-center items-center gap-5">
-            <Button title="Salvar" width="w-64" />
+            <Button title="Criar Conta" width="w-64" />
             <p
               className="text-lowGray cursor-pointer hover:text-darkPurple transition-colors duration-300 ease-in-out"
-              onClick={handleRedirectToRegister}
+              onClick={handleRedirectToLogin}
             >
-              Ainda não tenho uma conta
+              Já tenho uma conta
             </p>
           </div>
         </div>
--- traininSync-front/src/routes/routes.tsx
+++ traininSync-front/src/routes/routes.tsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 import App from "../App";
-import { RegisterPersonal } from "../pages/register-personal";
 import { NotFound } from "../pages/notFound";
 import { Login } from "../pages/login";
+import { RegisterPersonal } from "../pages/register-personal";
+import { Register } from "../pages/register";
 
 export function AppRoutes() {
   return (
@@ -11,6 +12,7 @@ export function AppRoutes() {
       <Route path="/" element={<App />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register/personal" element={<RegisterPersonal />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
