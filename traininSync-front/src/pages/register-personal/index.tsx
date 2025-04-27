35ab44304cb6bc2663d9bcce86fb0d97ceb74e29
Gabrielygor <gabrielcanuto042@gmail.com>
2025-04-27 17:32:53 -0300

    feat: add inputs fields and change the styles

--- traininSync-front/src/pages/register-personal/index.tsx
+++ traininSync-front/src/pages/register-personal/index.tsx
@@ -29,7 +29,7 @@ export function RegisterPersonal() {
         <Button onClick={handleLoginClick} title="Login" />
       </NavBar>
 
-      <div className="p-8">
+      <form className="p-8">
         <div className="flex justify-center gap-1.5 text-5xl font-black md:justify-start md:px-6 pb-6">
           <h1>Personal</h1>
           <h1 className="text-midPurple">List</h1>
@@ -45,31 +45,46 @@ export function RegisterPersonal() {
             <div className="grid grid-cols-1 md:grid-cols-4 gap-2 text-lowGray">
               <div className="flex flex-col gap-2 col-span-2">
                 <h2>Nome completo</h2>
-                <div className="h-11 bg-midGray rounded-xl"></div>
+                <input className="h-11 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none" />
               </div>
 
               <div className="flex flex-col gap-2 col-span-2">
                 <h2>Nome Social</h2>
-                <div className="h-11 bg-midGray rounded-xl"></div>
+                <input className="h-11 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none" />
               </div>
 
               <div className="flex flex-col gap-2 col-span-4">
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
                   <div className="col-span-1">
                     <h2>CPF</h2>
-                    <div className="h-11 bg-midGray rounded-xl"></div>
+                    <input className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none" />
                   </div>
                   <div className="col-span-1">
                     <h2>Etnia</h2>
-                    <div className="h-11 bg-midGray rounded-xl"></div>
+                    <select className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none">
+                      <option value="nao_informado">Não informado</option>
+                      <option value="amarela">Amarela</option>
+                      <option value="branca">Branca</option>
+                      <option value="indigena">Indigena</option>
+                      <option value="parda">Parda</option>
+                      <option value="preta">Preta</option>
+                    </select>
                   </div>
                   <div className="col-span-1">
                     <h2>Sexo</h2>
-                    <div className="h-11 bg-midGray rounded-xl"></div>
+                    <select className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none">
+                      <option value="N">Não informado</option>
+                      <option value="F">Feminino</option>
+                      <option value="M">Masculino</option>
+                      <option value="O">Outro</option>
+                    </select>
                   </div>
                   <div className="col-span-1">
                     <h2>Data de nascimento</h2>
-                    <div className="h-11 bg-midGray rounded-xl"></div>
+                    <input
+                      type="date"
+                      className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none"
+                    />
                   </div>
                 </div>
               </div>
@@ -78,11 +93,11 @@ export function RegisterPersonal() {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   <div className="col-span-1">
                     <h2>E-mail</h2>
-                    <div className="h-11 bg-midGray rounded-xl"></div>
+                    <input className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none" />
                   </div>
                   <div className="col-span-1">
                     <h2>Celular</h2>
-                    <div className="h-11 bg-midGray rounded-xl"></div>
+                    <input className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none" />
                   </div>
                 </div>
               </div>
@@ -91,11 +106,17 @@ export function RegisterPersonal() {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   <div className="col-span-1">
                     <h2>Estado Civil</h2>
-                    <div className="h-11 bg-midGray rounded-xl"></div>
+                    <select className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none">
+                      <option value="casado">Casado</option>
+                      <option value="divorciado">Divorciado</option>
+                      <option value="solteiro">Solteiro</option>
+                      <option value="uniao_estavel">União estável</option>
+                      <option value="viuvo">Viúvo</option>
+                    </select>
                   </div>
                   <div className="col-span-1">
                     <h2>CREF</h2>
-                    <div className="h-11 bg-midGray rounded-xl"></div>
+                    <input className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none" />
                   </div>
                 </div>
               </div>
@@ -113,11 +134,11 @@ export function RegisterPersonal() {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   <div className="col-span-1">
                     <h2>Número da Conta</h2>
-                    <div className="h-11 bg-midGray rounded-xl"></div>
+                    <input className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none" />
                   </div>
                   <div className="col-span-1">
                     <h2>Agencia</h2>
-                    <div className="h-11 bg-midGray rounded-xl"></div>
+                    <input className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none" />
                   </div>
                 </div>
               </div>
@@ -132,23 +153,23 @@ export function RegisterPersonal() {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-2 text-lowGray">
               <div className="flex flex-col gap-2 col-span-2">
                 <h2>Especialidades</h2>
-                <div className="h-24 bg-midGray rounded-xl"></div>
+                <textarea className="h-24 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none resize-none" />
               </div>
 
               <div className="flex flex-col gap-2 col-span-2">
                 <h2>Experiencia Profissional</h2>
-                <div className="h-24 bg-midGray rounded-xl"></div>
+                <textarea className="h-24 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none resize-none" />
               </div>
 
               <div className="flex flex-col gap-4 col-span-2 md:flex-row">
                 <div className="flex flex-col gap-2 col-span-2 md:col-span-1 md:w-1/2">
                   <h2>Horários Disponíveis</h2>
-                  <div className="h-24 bg-midGray rounded-xl"></div>
+                  <textarea className="h-24 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none resize-none" />
                 </div>
 
                 <div className="flex flex-col gap-2 col-span-2 md:col-span-1 md:w-1/2">
                   <h2>Locais Disponíveis</h2>
-                  <div className="h-24 bg-midGray rounded-xl"></div>
+                  <textarea className="h-24 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none resize-none" />
                 </div>
               </div>
             </div>
@@ -170,7 +191,7 @@ export function RegisterPersonal() {
             title="log Personais teste"
           />
         </div>
-      </div>
+      </form>
     </div>
   );
 }
