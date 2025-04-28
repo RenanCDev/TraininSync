bfc62dcfbfb0fe0886ac37f76ed2e9e836f5661f
Gabrielygor <gabrielcanuto042@gmail.com>
2025-04-28 08:42:51 -0300

    feat: add inputs and cleanData func

--- traininSync-front/src/pages/register-personal/index.tsx
+++ traininSync-front/src/pages/register-personal/index.tsx
@@ -2,17 +2,52 @@ import { useNavigate } from "react-router-dom";
 import { Button } from "../../components/button";
 import { NavBar } from "../../components/navbar";
 import { getAllPersonal } from "../../api/personal/getPersonal";
+import { CreatePersonal } from "./zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+type PersonalFormData = z.infer<typeof CreatePersonal>;
 
 export function RegisterPersonal() {
   const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PersonalFormData>({
+    resolver: zodResolver(CreatePersonal),
+  });
 
   function handleLoginClick() {
     navigate("/login");
   }
 
-  function handleSaveClick() {
-    console.log("Save");
-  }
+  const onSubmit = (data: PersonalFormData) => {
+    const cleanData = {
+      dados_bancarios: {
+        numero_conta: data.numero_conta,
+        agencia: data.agencia,
+      },
+      nome: data.nome,
+      cpf: data.cpf,
+      data_de_nascimento: data.data_de_nascimento,
+      email: data.email,
+      numero_de_celular: data.numero_de_celular,
+      sexo: data.sexo,
+      nome_social: data.nome_social || null,
+      etnia: data.etnia,
+      estado_civil: data.estado_civil,
+      status: true,
+      cref: data.cref,
+      especialidades: data.especialidades,
+      experiencia_profissional: data.experiencia_profissional,
+      horarios_disponiveis: data.horarios_disponiveis,
+      locais_disponiveis: data.locais_disponiveis,
+    };
+
+    console.log("Dados submetidos:", cleanData);
+  };
 
   async function getPersonais() {
     try {
@@ -29,7 +64,7 @@ export function RegisterPersonal() {
         <Button onClick={handleLoginClick} title="Login" />
       </NavBar>
 
-      <form className="p-8">
+      <form onSubmit={handleSubmit(onSubmit)} className="p-8">
         <div className="flex justify-center gap-1.5 text-5xl font-black md:justify-start md:px-6 pb-6">
           <h1>Personal</h1>
           <h1 className="text-midPurple">List</h1>
@@ -45,23 +80,49 @@ export function RegisterPersonal() {
             <div className="grid grid-cols-1 md:grid-cols-4 gap-2 text-lowGray">
               <div className="flex flex-col gap-2 col-span-2">
                 <h2>Nome completo</h2>
-                <input className="h-11 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none" />
+                <input
+                  type="text"
+                  {...register("nome")}
+                  className="h-11 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none"
+                />
+                {errors.nome && (
+                  <span className="text-red-500">{errors.nome.message}</span>
+                )}
               </div>
 
               <div className="flex flex-col gap-2 col-span-2">
                 <h2>Nome Social</h2>
-                <input className="h-11 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none" />
+                <input
+                  type="text"
+                  {...register("nome_social")}
+                  className="h-11 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none"
+                />
+                {errors.nome_social && (
+                  <span className="text-red-500">
+                    {errors.nome_social.message}
+                  </span>
+                )}
               </div>
 
               <div className="flex flex-col gap-2 col-span-4">
                 <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
                   <div className="col-span-1">
                     <h2>CPF</h2>
-                    <input className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none" />
+                    <input
+                      type="text"
+                      {...register("cpf")}
+                      className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none"
+                    />
+                    {errors.cpf && (
+                      <span className="text-red-500">{errors.cpf.message}</span>
+                    )}
                   </div>
                   <div className="col-span-1">
                     <h2>Etnia</h2>
-                    <select className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none">
+                    <select
+                      {...register("etnia")}
+                      className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none"
+                    >
                       <option value="nao_informado">Não informado</option>
                       <option value="amarela">Amarela</option>
                       <option value="branca">Branca</option>
@@ -69,22 +130,39 @@ export function RegisterPersonal() {
                       <option value="parda">Parda</option>
                       <option value="preta">Preta</option>
                     </select>
+                    {errors.cpf && (
+                      <span className="text-red-500">{errors.cpf.message}</span>
+                    )}
                   </div>
                   <div className="col-span-1">
                     <h2>Sexo</h2>
-                    <select className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none">
+                    <select
+                      {...register("sexo")}
+                      className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none"
+                    >
                       <option value="N">Não informado</option>
                       <option value="F">Feminino</option>
                       <option value="M">Masculino</option>
                       <option value="O">Outro</option>
                     </select>
+                    {errors.sexo && (
+                      <span className="text-red-500">
+                        {errors.sexo.message}
+                      </span>
+                    )}
                   </div>
                   <div className="col-span-1">
                     <h2>Data de nascimento</h2>
                     <input
                       type="date"
+                      {...register("data_de_nascimento")}
                       className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none"
                     />
+                    {errors.data_de_nascimento && (
+                      <span className="text-red-500">
+                        {errors.data_de_nascimento.message}
+                      </span>
+                    )}
                   </div>
                 </div>
               </div>
@@ -93,11 +171,29 @@ export function RegisterPersonal() {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   <div className="col-span-1">
                     <h2>E-mail</h2>
-                    <input className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none" />
+                    <input
+                      type="text"
+                      {...register("email")}
+                      className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none"
+                    />
+                    {errors.email && (
+                      <span className="text-red-500">
+                        {errors.email.message}
+                      </span>
+                    )}
                   </div>
                   <div className="col-span-1">
                     <h2>Celular</h2>
-                    <input className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none" />
+                    <input
+                      type="text"
+                      {...register("numero_de_celular")}
+                      className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none"
+                    />
+                    {errors.numero_de_celular && (
+                      <span className="text-red-500">
+                        {errors.numero_de_celular.message}
+                      </span>
+                    )}
                   </div>
                 </div>
               </div>
@@ -106,17 +202,34 @@ export function RegisterPersonal() {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   <div className="col-span-1">
                     <h2>Estado Civil</h2>
-                    <select className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none">
+                    <select
+                      {...register("estado_civil")}
+                      className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none"
+                    >
                       <option value="casado">Casado</option>
                       <option value="divorciado">Divorciado</option>
                       <option value="solteiro">Solteiro</option>
                       <option value="uniao_estavel">União estável</option>
                       <option value="viuvo">Viúvo</option>
                     </select>
+                    {errors.estado_civil && (
+                      <span className="text-red-500">
+                        {errors.estado_civil.message}
+                      </span>
+                    )}
                   </div>
                   <div className="col-span-1">
                     <h2>CREF</h2>
-                    <input className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none" />
+                    <input
+                      type="text"
+                      {...register("cref")}
+                      className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none"
+                    />
+                    {errors.cref && (
+                      <span className="text-red-500">
+                        {errors.cref.message}
+                      </span>
+                    )}
                   </div>
                 </div>
               </div>
@@ -134,11 +247,29 @@ export function RegisterPersonal() {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                   <div className="col-span-1">
                     <h2>Número da Conta</h2>
-                    <input className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none" />
+                    <input
+                      type="text"
+                      {...register("numero_conta")}
+                      className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none"
+                    />
+                    {errors.numero_conta && (
+                      <span className="text-red-500">
+                        {errors.numero_conta.message}
+                      </span>
+                    )}
                   </div>
                   <div className="col-span-1">
                     <h2>Agencia</h2>
-                    <input className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none" />
+                    <input
+                      type="text"
+                      {...register("agencia")}
+                      className="h-11 w-full bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none"
+                    />
+                    {errors.agencia && (
+                      <span className="text-red-500">
+                        {errors.agencia.message}
+                      </span>
+                    )}
                   </div>
                 </div>
               </div>
@@ -153,23 +284,55 @@ export function RegisterPersonal() {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-2 text-lowGray">
               <div className="flex flex-col gap-2 col-span-2">
                 <h2>Especialidades</h2>
-                <textarea className="h-24 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none resize-none" />
+                <textarea
+                  {...register("especialidades")}
+                  className="h-24 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none resize-none"
+                />
+                {errors.especialidades && (
+                  <span className="text-red-500">
+                    {errors.especialidades.message}
+                  </span>
+                )}
               </div>
 
               <div className="flex flex-col gap-2 col-span-2">
                 <h2>Experiencia Profissional</h2>
-                <textarea className="h-24 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none resize-none" />
+                <textarea
+                  {...register("experiencia_profissional")}
+                  className="h-24 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none resize-none"
+                />
+                {errors.experiencia_profissional && (
+                  <span className="text-red-500">
+                    {errors.experiencia_profissional.message}
+                  </span>
+                )}
               </div>
 
               <div className="flex flex-col gap-4 col-span-2 md:flex-row">
                 <div className="flex flex-col gap-2 col-span-2 md:col-span-1 md:w-1/2">
                   <h2>Horários Disponíveis</h2>
-                  <textarea className="h-24 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none resize-none" />
+                  <textarea
+                    {...register("horarios_disponiveis")}
+                    className="h-24 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none resize-none"
+                  />
+                  {errors.horarios_disponiveis && (
+                    <span className="text-red-500">
+                      {errors.horarios_disponiveis.message}
+                    </span>
+                  )}
                 </div>
 
                 <div className="flex flex-col gap-2 col-span-2 md:col-span-1 md:w-1/2">
                   <h2>Locais Disponíveis</h2>
-                  <textarea className="h-24 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none resize-none" />
+                  <textarea
+                    {...register("locais_disponiveis")}
+                    className="h-24 bg-midGray rounded-xl p-2 focus:border text-white focus:border-lowGray outline-none resize-none"
+                  />
+                  {errors.locais_disponiveis && (
+                    <span className="text-red-500">
+                      {errors.locais_disponiveis.message}
+                    </span>
+                  )}
                 </div>
               </div>
             </div>
@@ -177,11 +340,7 @@ export function RegisterPersonal() {
         </div>
 
         <div className="mt-7">
-          <Button
-            onClick={handleSaveClick}
-            width="w-full max-w-[342px]"
-            title="Salvar"
-          />
+          <Button type="submit" width="w-full max-w-[342px]" title="Salvar" />
         </div>
 
         <div className="mt-7">
