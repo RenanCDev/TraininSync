49c3f8ccd6747ae72f73c1a43b9490be7858d906
Gabrielygor <gabrielcanuto042@gmail.com>
2025-04-30 21:23:38 -0300

    feat: add loading button animation

--- traininSync-front/src/components/button/index.tsx
+++ traininSync-front/src/components/button/index.tsx
@@ -1,13 +1,17 @@
+import { Loader2 } from "lucide-react";
+
 interface ButtonProps {
   title: string;
   height?: string;
   width?: string;
+  loading?: boolean;
   type?: "button" | "submit" | "reset" | undefined;
   onClick?: () => void;
 }
 
 export function Button({
   title,
+  loading = false,
   type = "button",
   height = "h-auto",
   width = "w-auto",
@@ -15,11 +19,14 @@ export function Button({
 }: ButtonProps) {
   return (
     <button
+      disabled={loading}
       onClick={onClick}
-      className={`bg-midPurple cursor-pointer flex justify-center items-center p-3 text-2xl rounded-2xl transition duration-300 ease-in-out hover:scale-105 hover:bg-darkPurple z-20 ${height} ${width}`}
+      className={`bg-midPurple cursor-pointer flex justify-center items-center p-3 text-2xl rounded-2xl transition duration-300 ease-in-out hover:scale-105 hover:bg-darkPurple z-20 ${
+        loading ? "cursor-not-allowed opacity-50" : "cursor-pointer"
+      } ${height} ${width}`}
       type={type}
     >
-      {title}
+      {loading ? <Loader2 className="h-8 w-8 animate-spin" /> : title}
     </button>
   );
 }
