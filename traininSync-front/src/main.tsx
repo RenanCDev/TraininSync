212525af7afb67063c917a9418c7e5588ade7dc8
Gabrielygor <gabrielcanuto042@gmail.com>
2025-04-30 21:38:28 -0300

    feat: add react toasty container

--- traininSync-front/src/main.tsx
+++ traininSync-front/src/main.tsx
@@ -1,11 +1,14 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes/routes.tsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./global.css";
 import "./tailwind.css";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <AppRoutes />
+    <ToastContainer autoClose={3000} />
   </BrowserRouter>
 );
