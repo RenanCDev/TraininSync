f76a990f463c725478c72ed14471b2340770df80
Rael Araujo <131907370+raelaraujo0@users.noreply.github.com>
2025-05-18 14:48:27 -0300

    revert: default settings
    Signed-off-by: Rael Araujo  <131907370+raelaraujo0@users.noreply.github.com>

--- traininSync-back/setup/settings.py
+++ traininSync-back/setup/settings.py
@@ -107,22 +107,12 @@ WSGI_APPLICATION = "setup.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "traininsync",
-        "USER": "rael",
-        "PASSWORD": "senha",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
-}
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.getenv("DATABASE_URL")
-#     )
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
+}
 
 
 # Password validation
