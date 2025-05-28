485935b15668c742e1ed6f577e97227deae3df06
Gabrielygor <gabrielcanuto042@gmail.com>
2025-05-28 17:22:49 -0300

    feat: add schedule route

--- traininSync-front/src/routes/routes.tsx
+++ traininSync-front/src/routes/routes.tsx
@@ -5,6 +5,7 @@ import { Login } from "../pages/login";
 import { RegisterPersonal } from "../pages/register-personal";
 import { Register } from "../pages/register";
 import { RegisterStudent } from "../pages/register-student";
+import { RegisterSchedule } from "../pages/schedule/register-schedule";
 
 export function AppRoutes() {
   return (
@@ -12,9 +13,10 @@ export function AppRoutes() {
       <Route path="*" element={<NotFound />} />{" "}
       <Route path="/" element={<App />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/register/personal" element={<RegisterPersonal />} />
       <Route path="/register/student" element={<RegisterStudent />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/register/schedule" element={<RegisterSchedule />} />
     </Routes>
   );
 }
