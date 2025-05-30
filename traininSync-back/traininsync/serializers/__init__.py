b3ff73f72d117bd1cd69030d0dd06bc15c4bc94d
raelaraujo0 <raellyedson1@gmail.com>
2025-05-29 21:09:30 -0300

    feat: call in init and create a serializer for register #115

--- traininSync-back/traininsync/serializers/__init__.py
+++ traininSync-back/traininsync/serializers/__init__.py
@@ -1,9 +1,12 @@
+"""Inicialização do pacote de serializers."""
+
 from .agendaserializer import AgendaSerializer
 from .alunoserializer import AlunoSerializer
 from .contratodeservicoserializer import ContratoDeServicoSerializer
 from .dadosbancariosserializers import DadosBancariosSerializer
+from .pagamentoserializer import PagamentoSerializer
 from .personalserializer import PersonalSerializer
 from .pessoaserializers import PessoaSerializer
+from .register_serializer import RegisterSerializer
 from .registrodeprogressoserializer import RegistroDeProgressoSerializer
 from .servicoserializer import ServicoSerializer
-from .pagamentoserializer import PagamentoSerializer
