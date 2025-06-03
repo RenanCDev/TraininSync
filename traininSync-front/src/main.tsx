bd47f8ff8fdde6479d0c79ae57740ff27cfc9361
Gabrielygor <gabrielcanuto042@gmail.com>
2025-06-03 14:06:18 -0300

    feat: back to top button

--- traininSync-front/src/main.tsx
+++ traininSync-front/src/main.tsx
@@ -5,10 +5,12 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./global.css";
 import "./tailwind.css";
+import { ScrollToTopButton } from "./components/back-to-top-button/index.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <AppRoutes />
     <ToastContainer autoClose={3000} />
+    <ScrollToTopButton />
   </BrowserRouter>
 );
