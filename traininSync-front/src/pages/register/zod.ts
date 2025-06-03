ef9af4f1245485ca2c105970e97a2fdcb1ba1261
Gabrielygor <gabrielcanuto042@gmail.com>
2025-06-03 13:55:27 -0300

    feat: add email in register screen

--- traininSync-front/src/pages/register/zod.ts
+++ traininSync-front/src/pages/register/zod.ts
@@ -5,6 +5,7 @@ export const RegisterZod = z
     usuario: z.string().min(3, "Digite um nome de usuário válido"),
     senha: z.string().min(1, "Digite a senha"),
     confirmarSenha: z.string().min(1, "Digite a senha"),
+    email: z.string().email("E-mail inválido"),
   })
   .refine((data) => data.senha === data.confirmarSenha, {
     message: "As senhas devem ser iguais",
