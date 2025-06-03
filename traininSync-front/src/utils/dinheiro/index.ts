aaaf7c5e4f486dcd359923c220d3a1606a847b82
Gabrielygor <gabrielcanuto042@gmail.com>
2025-06-03 18:59:50 -0300

    feat: add format money to BRL func

--- traininSync-front/src/utils/dinheiro/index.ts
+++ traininSync-front/src/utils/dinheiro/index.ts
@@ -1,17 +1,13 @@
-export function formatToRealMoney(valor: string | number): string {
-  const numero =
-    typeof valor === "number" ? valor : Number(valor.replace(/\D/g, "")) / 100;
-  return numero.toLocaleString("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  });
+export function formatCurrency(value: number | string): string {
+  const number =
+    typeof value === "string"
+      ? parseFloat(value.replace(/[^\d,]/g, "").replace(",", "."))
+      : value;
+  if (isNaN(number)) return "";
+  return number.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
 }
 
-export function removerFormatacaoReal(valorFormatado: string): string {
-  return valorFormatado
-    .replace(/\s/g, "")
-    .replace("R$", "")
-    .replace(/\./g, "")
-    .replace(",", ".")
-    .trim();
+export function unformatCurrency(value: string): number {
+  const numeric = value.replace(/[^\d,]/g, "").replace(",", ".");
+  return parseFloat(numeric);
 }
