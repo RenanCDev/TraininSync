d236dd8f42b66d67c9accc68b0da8af58818fc1c
luizmiguel4444 <luizmiguel4444@gmail.com>
2025-07-10 17:34:31 -0300

    feat: add new modifications to project

--- traininSync-front/src/routes/routes.tsx
+++ traininSync-front/src/routes/routes.tsx
@@ -9,20 +9,35 @@ import { RegisterService } from "../pages/service/register-service";
 import { RegisterPayment } from "../pages/payment/register-payment";
 import { RegisterProgress } from "../pages/progress/register-progress";
 import { RegisterPersonal } from "../pages/personal/register-personal";
+import { StudentTable } from "../pages/student/get-student";
+import { EditStudent } from "../pages/student/update-student";
+import { ContractTable } from "../pages/contract/get-contract";
+import { RegisterContract } from "../pages/contract/register-contract";
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="*" element={<NotFound />} />{" "}
+      <Route path="*" element={<NotFound />} />
       <Route path="/" element={<App />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+
       <Route path="/register/personal" element={<RegisterPersonal />} />
       <Route path="/register/student" element={<RegisterStudent />} />
       <Route path="/register/schedule" element={<RegisterSchedule />} />
       <Route path="/register/service" element={<RegisterService />} />
       <Route path="/register/payment" element={<RegisterPayment />} />
       <Route path="/register/progress" element={<RegisterProgress />} />
+      <Route path="/register/contract" element={<RegisterContract />} />
+
+      <Route path="/student" element={<StudentTable />} />
+      <Route path="/contract" element={<ContractTable />} />
+      {/* <Route path="/schedule" element={<ScheduleTable />} />
+      <Route path="/service" element={<ServiceTable />} />
+      <Route path="/payment" element={<PaymentTable />} />
+      <Route path="/progress" element={<ProgressTable />} /> */}
+
+      <Route path="/edit-student/:id" element={<EditStudent />} />
     </Routes>
   );
 }
